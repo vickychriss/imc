e9ead60c2af5014b64f24a2df4016b8366bfeb34
vickychriss <vitoriacristinemarianomelo3@gmail.com>
2024-03-19 20:23:26 -0300

    first commit

---
+++ po.java
@@ -0,0 +1,23 @@
+ import java.util.Scanner;
+     public class po{
+        public static void main(Strings[]args){
+          float imc,altura,peso;
+          Scanner te=new Scanner(System.in);
+          System.out.printnl("Digite sua altura:");
+          altura = te.nextFloat();
+          System.out.printnl("Digite seu peso:");
+          peso = te.nextFloat();
+          imc = peso(altura * altura);
+          if(imc < 18.5){
+           System.out.printnl("Abaixo do peso");
+          }
+          else if(imc > 18.5 && imc < 25){
+            System.out.printnl("Peso normal");
+          }
+          else if() {
+
+          }
+
+
+  }
+}
